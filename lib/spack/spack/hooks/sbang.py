29a12ca2f74efdf2d86a195513cd42935ab91942
Gregory Becker <becker33@llnl.gov>
2025-04-16 10:32:01 -0700

    sbang: make sbang install process safe
    Signed-off-by: Gregory Becker <becker33@llnl.gov>

--- lib/spack/spack/hooks/sbang.py
+++ lib/spack/spack/hooks/sbang.py
@@ -11,6 +11,7 @@ import sys
 import tempfile
 
 import llnl.util.filesystem as fs
+import llnl.util.lock
 import llnl.util.tty as tty
 
 import spack.error
@@ -189,38 +190,47 @@ def install_sbang():
     if os.path.exists(sbang_path) and filecmp.cmp(spack.paths.sbang_script, sbang_path):
         return
 
-    # make $install_tree/bin
-    sbang_bin_dir = os.path.dirname(sbang_path)
-    fs.mkdirp(sbang_bin_dir)
+    lock_path = spack.store.STORE.db.database_directory / "sbang_lock"
+    lock = llnl.util.lock.Lock(lock_path, default_timeout=10, desc="sbang install lock")
+    with llnl.util.lock.WriteTransaction(lock):
+        # If it was created while waiting for the lock, exit
+        if os.path.exists(sbang_path) and filecmp.cmp(spack.paths.sbang_script, sbang_path):
+            return
 
-    # get permissions for bin dir from configuration files
-    group_name = spack.package_prefs.get_package_group(spack.spec.Spec("all"))
-    config_mode = spack.package_prefs.get_package_dir_permissions(spack.spec.Spec("all"))
+        # make $install_tree/bin
+        sbang_bin_dir = os.path.dirname(sbang_path)
+        fs.mkdirp(sbang_bin_dir)
 
-    if group_name:
-        os.chmod(sbang_bin_dir, config_mode)  # Use package directory permissions
-    else:
-        fs.set_install_permissions(sbang_bin_dir)
+        # get permissions for bin dir from configuration files
+        group_name = spack.package_prefs.get_package_group(spack.spec.Spec("all"))
+        config_mode = spack.package_prefs.get_package_dir_permissions(spack.spec.Spec("all"))
 
-    # set group on sbang_bin_dir if not already set (only if set in configuration)
-    # TODO: after we drop python2 support, use shutil.chown to avoid gid lookups that
-    # can fail for remote groups
-    if group_name and os.stat(sbang_bin_dir).st_gid != grp.getgrnam(group_name).gr_gid:
-        os.chown(sbang_bin_dir, os.stat(sbang_bin_dir).st_uid, grp.getgrnam(group_name).gr_gid)
+        if group_name:
+            os.chmod(sbang_bin_dir, config_mode)  # Use package directory permissions
+        else:
+            fs.set_install_permissions(sbang_bin_dir)
 
-    # copy over the fresh copy of `sbang`
-    sbang_tmp_path = os.path.join(
-        os.path.dirname(sbang_path), ".%s.tmp" % os.path.basename(sbang_path)
-    )
-    shutil.copy(spack.paths.sbang_script, sbang_tmp_path)
+        # set group on sbang_bin_dir if not already set (only if set in configuration)
+        # TODO: after we drop python2 support, use shutil.chown to avoid gid lookups that
+        # can fail for remote groups
+        if group_name and os.stat(sbang_bin_dir).st_gid != grp.getgrnam(group_name).gr_gid:
+            os.chown(sbang_bin_dir, os.stat(sbang_bin_dir).st_uid, grp.getgrnam(group_name).gr_gid)
 
-    # set permissions on `sbang` (including group if set in configuration)
-    os.chmod(sbang_tmp_path, config_mode)
-    if group_name:
-        os.chown(sbang_tmp_path, os.stat(sbang_tmp_path).st_uid, grp.getgrnam(group_name).gr_gid)
+        # copy over the fresh copy of `sbang`
+        sbang_tmp_path = os.path.join(
+            os.path.dirname(sbang_path), ".%s.tmp" % os.path.basename(sbang_path)
+        )
+        shutil.copy(spack.paths.sbang_script, sbang_tmp_path)
 
-    # Finally, move the new `sbang` into place atomically
-    os.rename(sbang_tmp_path, sbang_path)
+        # set permissions on `sbang` (including group if set in configuration)
+        os.chmod(sbang_tmp_path, config_mode)
+        if group_name:
+            os.chown(
+                sbang_tmp_path, os.stat(sbang_tmp_path).st_uid, grp.getgrnam(group_name).gr_gid
+            )
+
+        # Finally, move the new `sbang` into place atomically
+        os.rename(sbang_tmp_path, sbang_path)
 
 
 def post_install(spec, explicit=None):
