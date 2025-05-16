fa4eada00f8b696e268e3f94d1b633cdad47b4c2
Kathleen Shea <shea9@llnl.gov>
2025-05-16 12:49:26 -0700

    moving mock pkgs to test_repos

---
+++ var/spack/test_repos/builtin.mock/packages/parallel-package-c/package.py
@@ -0,0 +1,23 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import time
+
+from llnl.util.filesystem import touch
+
+from spack.package import *
+
+
+class ParallelPackageC(Package):
+    """This is a fake vtk-m package used to demonstrate virtual package providers
+    with dependencies."""
+
+    homepage = "http://www.example.com"
+    has_code = False
+
+    version("1.0")
+
+    def install(self, spec, prefix):
+        time.sleep(2)
+        touch(prefix.dummy_file)
